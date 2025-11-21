e81e06c05fb9899a8595c864940c7fc1fe692d96
Federico Améndola <famendola@alumnos.exa.unicen.edu.ar>
2025-11-21 09:27:43 -0300

    Cambio de seguridad + Incorporacion Compocli #2

---
+++ AyJ-ETLs/AyJ-ETLs/patch scripts/002_nuevas_tablas_banka.sql
@@ -0,0 +1,30 @@
+﻿USE [AyJStaging]
+GO
+
+/****** Object:  Table [dbo].[SALDOSTARJETAS]    Script Date: 11/12/2025 4:10:48 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SALDOSTARJETAS](
+	[TARJETA] [float] NULL,
+	[APELLIDO] [varchar](40) NULL,
+	[NOMBRE] [varchar](40) NULL,
+	[ID] [float] NULL,
+	[ID_EMPRESA] [float] NULL,
+	[ID_SUCURSAL] [float] NULL,
+	[AUTORIZACIONES] [float] NULL,
+	[DEBITOS] [float] NULL,
+	[CREDITOS] [float] NULL,
+	[COMPENSATORIOS] [float] NULL,
+	[PUNITORIOS] [float] NULL,
+	[CARGOS_EMISION] [float] NULL,
+	[CARGOS_RENOVACION] [float] NULL,
+	[ULT_LIQ] [float] NULL,
+	[FUTURA] [float] NULL,
+	[COMPRAS] [float] NULL,
+	[NOMBRE_SUCURSAL] [varchar](60) NULL
+) ON [PRIMARY]
+GO
