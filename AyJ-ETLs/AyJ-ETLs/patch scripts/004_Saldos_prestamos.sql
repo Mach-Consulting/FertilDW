db928191fd2be5ff65602e5deddc745bd1d493c8
Federico Améndola <famendola@alumnos.exa.unicen.edu.ar>
2026-01-10 22:12:30 -0300

    Saldo Liq Préstamo: Total por último día #4

--- AyJ-ETLs/AyJ-ETLs/patch scripts/004_Saldos_prestamos.sql
+++ AyJ-ETLs/AyJ-ETLs/patch scripts/004_Saldos_prestamos.sql
@@ -1,7 +1,7 @@
 USE [AyJDW]
 GO
 
-/****** Object:  Table [dbo].[SaldoLiquidacionPrestamo]    Script Date: 1/6/2026 10:29:03 AM ******/
+/****** Object:  Table [dbo].[SaldoLiquidacionPrestamo]    Script Date: 1/10/2026 10:06:20 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -10,68 +10,21 @@ GO
 
 CREATE TABLE [dbo].[SaldoLiquidacionPrestamo](
 	[Cod_Tabla] [float] NULL,
-	[Fecha] [date] NULL,
+	[Fecha] [datetime] NULL,
 	[Apellido] [varchar](50) NULL,
 	[Nombre] [varchar](50) NULL,
 	[Cod_Usuario] [float] NULL,
-	[Cod_Empresa] [float] NULL,
-	[Cod_Sucursal] [float] NULL,
-	[Cod_Autorizaciones] [float] NULL,
 	[Debitos] [float] NULL,
 	[Creditos] [float] NULL,
-	[Compensatorios] [float] NULL,
-	[Punitorios] [float] NULL,
-	[Cargos_Emision] [float] NULL,
-	[Cargos_Renovacion] [float] NULL,
-	[Ult_Liq] [float] NULL,
 	[Futura] [float] NULL,
-	[Compras] [float] NULL,
 	[Nombre_Sucursal] [varchar](40) NULL,
 	[Saldo] [float] NULL,
-	[Prestamos] [float] NULL,
-	[Nota] [varchar](200) NULL,
-	[Nombre_Canal_Cobro] [varchar](40) NULL,
-	[Tarjeta] [float] NULL,
-	[Fecha_Original] [datetime] NULL
+	[Fecha_Filtro] [date] NULL,
+	[Interes_Saldo] [float] NULL,
+	[Cargos] [float] NULL,
+	[Saldo_Total] [float] NULL,
+	[Cap_Futuro] [float] NULL,
+	[Int_Futuro] [float] NULL,
+	[Saldo_Liq] [float] NULL
 ) ON [PRIMARY]
 GO
-
-USE [AyJStaging]
-GO
-
-/****** Object:  Table [dbo].[SALDO_TARJETA_CIERRE]    Script Date: 1/6/2026 8:47:53 PM ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[SALDO_TARJETA_CIERRE](
-	[ID_TABLA] [float] NULL,
-	[FECHA] [datetime] NULL,
-	[APELLIDO] [varchar](50) NULL,
-	[NOMBRE] [varchar](50) NULL,
-	[ID] [float] NULL,
-	[ID_EMPRESA] [float] NULL,
-	[ID_SUCURSAL] [float] NULL,
-	[ID_AUTORIZACIONES] [float] NULL,
-	[DEBITOS] [float] NULL,
-	[CREDITOS] [float] NULL,
-	[COMPENSATORIOS] [float] NULL,
-	[PUNITORIOS] [float] NULL,
-	[CARGOS_EMISION] [float] NULL,
-	[CARGOS_RENOVACION] [float] NULL,
-	[ULT_LIQ] [float] NULL,
-	[FUTURA] [float] NULL,
-	[COMPRAS] [float] NULL,
-	[NOMBRE_SUCURSAL] [varchar](40) NULL,
-	[SALDO] [float] NULL,
-	[PRESTAMOS] [float] NULL,
-	[NOTA] [varchar](200) NULL,
-	[NOMBRE_CANAL_COBRO] [varchar](40) NULL,
-	[TARJETA] [float] NULL
-) ON [PRIMARY]
-GO
-
-
-
