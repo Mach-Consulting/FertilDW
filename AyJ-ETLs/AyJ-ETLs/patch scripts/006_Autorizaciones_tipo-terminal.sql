858e73c4d9ce7a5b98745b033e8d2fb2a67e6a0a
Federico Améndola <famendola@alumnos.exa.unicen.edu.ar>
2026-01-19 11:53:20 -0300

    Autorizaciones: Codigos de Rubros y SubRubros #6

--- AyJ-ETLs/AyJ-ETLs/patch scripts/006_Autorizaciones_tipo-terminal.sql
+++ AyJ-ETLs/AyJ-ETLs/patch scripts/006_Autorizaciones_tipo-terminal.sql
@@ -1,3 +1,6 @@
 ALTER TABLE FactAutorizacionesTarjetas 
 ADD Tipo nvarchar(1), Terminal nvarchar(20), Terminal_Pos nvarchar(20), Mensaje_Pos nvarchar(30)
 GO
+
+ALTER TABLE DimComercio ADD Cod_Rubro numeric(10, 0), Cod_SubRubro numeric(10,0)
+GO
