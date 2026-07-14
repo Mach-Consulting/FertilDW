069df7932a5d6401f77aaad83f17835a158d5cb6
Federico Améndola <famendola@alumnos.exa.unicen.edu.ar>
2026-07-14 11:05:22 -0300

    Nuevo FactCobros #13

---
+++ AyJ-ETLs/AyJ-ETLs/patch scripts/010_FactCobros_patch.sql
@@ -0,0 +1,8 @@
+ALTER TABLE FactCobros DROP COLUMN Fecha_Vencimiento
+GO
+ALTER TABLE FactCobros DROP COLUMN Cod_Producto
+GO
+ALTER TABLE FactCobros DROP COLUMN Tipo_Comprobante
+GO
+ALTER TABLE FactCobros DROP COLUMN Factura
+GO
